e1fdc9d14ad04ed88186eda3efa055ac81e23340
aimeiyijia <1456723200@qq.com>
2020-03-30 21:56:56 +0800

    添加依赖文件

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import login from '../views/login'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'login',
+        component: login
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: function () {
+            return import(/* webpackChunkName: "about" */ '../views/About.vue')
+        }
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
